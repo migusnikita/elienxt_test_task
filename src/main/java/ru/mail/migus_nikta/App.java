614abffd96640a01fe43c9773020f829a365f785
migus nikita <migus_nikita@mail.ru>
2020-11-20 14:02:28 +0300

    commit new project

---
+++ src/main/java/ru/mail/migus_nikta/App.java
@@ -0,0 +1,13 @@
+package ru.mail.migus_nikta;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/ru/mail/migus_nikta/AppTest.java
@@ -0,0 +1,20 @@
+package ru.mail.migus_nikta;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
